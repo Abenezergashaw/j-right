7103c2bb3a957425aa8aef0b50dd94a508b9963a
cherent tsegaye <abenigas59@gmail.com>
2026-01-30 16:43:52 +0300

    first commit

--- app/composables/useUrl.js
+++ app/composables/useUrl.js
@@ -1,4 +1,5 @@
-const url = "http://localhost:3030";
+const url = "http://api.rightbet.org";
+// const url = "http://localhost:3030";
 // const url = "http://192.168.1.4:3030";
 
 export function useUrl() {
